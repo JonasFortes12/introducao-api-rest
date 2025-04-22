bfafb0155dd6af4f52a8a48255ee6c8de73cb998
Jonas Fortes <jonascforte@hotmail.com>
2025-04-22 16:17:15 -0300

    refact|feat: multi routes de usuários e fornecedores

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express from 'express'
 
+import usuariosRoutes from './routes/usuarios.js'
+import fornecedoresRoutes from "./routes/fornecedores.js"
+
 const app = express()
+const port = 3000
 
 // Permitir ler JSON no corpo da requisição
 app.use(express.json())
 
-const port = 3000
+// Adiciona o roteador de usuários
+app.use("/usuarios", usuariosRoutes)
 
-// Banco de dados Fake (em memória)
-const usuarios = [
-    {id: 1, nome: 'João', email:"joao@email.com"},
-    {id: 2, nome: 'Ana', email:"ana@email.com"}
-]
+// Adiciona o roteador de fornecedores
+app.use("/fornecedores", fornecedoresRoutes)
 
 
 // Rota início
@@ -20,90 +22,6 @@ app.get("/", (req, res)=>{
     res.send("Bem vindo a minha API!")
 
 })
-
-//Rota cria usuário
-app.post("/criarUsuario", (req, res) => {
-    const { nome, email } = req.body;
-
-    const novoUsuario = {
-        id: usuarios[usuarios.length-1].id + 1,
-        nome: nome,
-        email: email
-    }
-
-    usuarios.push(novoUsuario)
-
-    res.status(201).send(usuarios)
-})
-
-app.put("/usuario/:id", (req, res)=>{
-    const { id } = req.params
-    const {novoNome, novoEmail} = req.body
-
-    const indice = usuarios.findIndex((usuario)=>{
-        return usuario.id == id
-    })
-
-    //if indice === -1, dê como resposta da requisição 
-    // o status 404
-    if(indice === -1){
-        return res.status(404).json(
-            {mensagem: "Usuário não encontrado!"})
-    }
-
-    usuarios[indice].nome = novoNome
-    usuarios[indice].email = novoEmail
-
-    res.send(usuarios)
-
-})
-
-
-/** 
- * Crie uma rota(endpoint) do tipo GET com URI: /usuarios 
- * que envie uma resposta (mensagem) com todos os 
- * usuários cadastrados no "banco de dados fake".
-*/
-app.get("/usuarios", (req, res) => {
-
-    // res.json(usuarios)
-    res.status(200).json(usuarios)
-
-})
-
-
-/**
- * Faça uma rota para deletar um usuário de acordo com o id
- * recebido por parâmetro
- *  Método: DELETE
- *  Endpoint: /usuario/:id
- * 
- *  Resposta: a lista de usuários atualizada
- * 
- * dica: use o splice() para remover o usuário da lista
- */
-
-app.delete("/usuarios/:id", (req, res)=>{
-    //const id = req.params.id
-    const { id } = req.params
-
-    const index = usuarios.findIndex((usuario)=>{
-        return usuario.id == parseInt(id)
-    })
-
-    if (index === -1) {
-        res.status(404).json(
-            {mensagem: "Usuário não encontrado!"})
-    }else{
-        usuarios.splice(index, 1)
-        res.send(usuarios)
-    }
-
-})
-
-
-
-
 
 
 
